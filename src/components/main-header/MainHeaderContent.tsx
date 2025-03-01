812545a102edd4821babd48c4ccf5207a321f252
kenebebh <kenebebhbanigo@gmail.com>
2025-03-01 01:24:09 +0100

    finished the header component

--- src/components/main-header/MainHeaderContent.tsx
+++ src/components/main-header/MainHeaderContent.tsx
@@ -1,30 +1,43 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { ChevronRight, Tags } from "lucide-react";
 
 export function MainHeaderContent() {
   return (
-    <div className="flex flex-col md:flex-row md:space-x-24">
-      <div className="flex-1 max-w-xl -mt-8 px-6 flex justify-center flex-col">
+    <div className="flex flex-col justify-center items-center pt-16">
+      <div className="flex-1 max-w-6xl px-6 flex justify-center  items-center flex-col py-16">
+        <Button className="bg-ring w-fit rounded-full hover:bg-muted-foreground/50">
+          <div className="flex items-center gap-x-1">
+            <Tags color="#636363" />
+            <p className="text-accent-text font-light">
+              Promo until 28th February to save 20%
+            </p>
+            <ChevronRight color="#3B3956" strokeWidth={4} />
+          </div>
+        </Button>
         <h1
-          className="text-4xl md:text-5xl font-semibold mb-4 border-l pl-3"
+          className="text-4xl md:text-6xl font-semibold mb-4 text-accent-text pt-10"
           id="main-heading"
         >
           Your Best Personal Health Solution
         </h1>
-        <p className="text-lg mb-6 font-medium pl-3">
+        <p className="text-lg mb-8 font-medium text-accent-text-2 max-w-2xl text-center">
           Chat with doctors, visit nearest hospitals, buy your medicine, check
           on labs and update your information about health, all in one package!
         </p>
         <Button
-          //   size="lg"
-          className="bg-primary text-primary-foreground hover:bg-primary/90 w-[300px] rounded-full ml-3"
+          // size="lg"
+          className="bg-primary py-6 text-primary-foreground hover:bg-primary/90 w-[300px] rounded-full text-lg"
         >
-          Book Appointment
+          <div className="flex items-center gap-x-3">
+            Book Appointment
+            <ChevronRight color="#fff" />
+          </div>
         </Button>
       </div>
       <div className="flex-1 h-[600px]">
         <Image
-          src="/webapp-image.png"
+          src="/webapp-img.png"
           alt="Doctor appointment booking application interface"
           width={1000}
           height={800}
--- src/components/main-header/index.tsx
+++ src/components/main-header/index.tsx
@@ -4,7 +4,8 @@ import { AnimateWrapper } from "@/helpers/animations/animateWrapper";
 
 export default function MainHeader() {
   return (
-    <header className="min-h-screen p-6 md:p-12 md:pr-0 flex items-center">
+    // <header className="min-h-screen flex flex-col items-center justify-center p-6 md:p-12 bg-gradient-to-br from-white-200 via-blue-200 to-pink-200">
+    <header className="min-h-screen flex flex-col items-center justify-center p-6 md:p-12 bg-linear-to-br[white_25%,blue_20%,purple_20%,pink_20%,teal]">
       {/* <AnimateWrapper variant="slideUp" duration={0.5}> */}
       <MainHeaderAnimation>
         <MainHeaderContent />
