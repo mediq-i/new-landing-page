c02641ff1689a5bb6deb48e726646e448bfc2b48
kenebebh <kenebebhbanigo@gmail.com>
2025-03-03 02:44:15 +0100

    created footer icon components

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -5,5 +5,21 @@ import Facebook from "./facebook";
 import Instagram from "./instagram";
 import Linkedin from "./linkedin";
 import Twitter from "./twitter";
+import FooterFacebook from "./footer-facebook";
+import FooterInstagram from "./footer-instagram";
+import FooterLinkedin from "./footer-linkedin";
+import FooterTwitter from "./footer-twitter";
 
-export { Doctor, Hospital, Medicine, Facebook, Instagram, Linkedin, Twitter };
+export {
+  Doctor,
+  Hospital,
+  Medicine,
+  Facebook,
+  Instagram,
+  Linkedin,
+  Twitter,
+  FooterFacebook,
+  FooterInstagram,
+  FooterLinkedin,
+  FooterTwitter,
+};
