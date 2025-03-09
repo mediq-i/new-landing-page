6fccca7d9aee51d13d015f10c9fc69414944e142
kenebebh <kenebebhbanigo@gmail.com>
2025-03-09 17:33:16 +0100

    fixed build error caused by next id page type

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,3 +1,23 @@
+// import { notFound } from "next/navigation";
+// import { articles } from "@/components/partials/data";
+// import { BlogDetail } from "@/components/blog-page-components";
+
+// export function generateStaticParams() {
+//   return articles.map((article) => ({
+//     id: article.id,
+//   }));
+// }
+
+// export default function BlogPostPage({ params }: { params: { id: string } }) {
+//   const article = articles.find((article) => article.id === params.id);
+
+//   if (!article) {
+//     notFound();
+//   }
+
+//   return <BlogDetail article={article} />;
+// }
+
 import { notFound } from "next/navigation";
 import { articles } from "@/components/partials/data";
 import { BlogDetail } from "@/components/blog-page-components";
@@ -8,8 +28,14 @@ export function generateStaticParams() {
   }));
 }
 
-export default function BlogPostPage({ params }: { params: { id: string } }) {
-  const article = articles.find((article) => article.id === params.id);
+export default async function BlogPostPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const article = articles.find((article) => article.id === id);
 
   if (!article) {
     notFound();
